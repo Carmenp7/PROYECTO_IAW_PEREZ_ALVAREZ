dfa587b9941a9c8bdae5c3907611807986417586
Carmenp7 <mcpa1488@gmail.com>
2019-02-25 22:27:41 +0100

    25.02.2018

--- Proyecto Final/INICIO.php
+++ Proyecto Final/INICIO.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+i<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,9 +12,54 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 </head>  
 <body> 
-<div class="container-fluid">
+<div class="container">
+  <section class="section-white">
+  <div class="container">
+
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+      </ol>
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="http://placehold.it/600x200" alt="...">
+          <div class="carousel-caption">
+            <h2>Heading</h2>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://placehold.it/600x200" alt="...">
+          <div class="carousel-caption">
+            <h2>Heading</h2>
+          </div>
+        </div>
+        <div class="item">
+          <img src="http://placehold.it/600x200" alt="...">
+          <div class="carousel-caption">
+            <h2>Heading</h2>
+          </div>
+        </div>
+      </div>
+
+      <!-- Controls -->
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
+    </div>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <a class="navbar-brand" href="principal.php">AUTOINYECCION DIESEL</a>
   <ul class="navbar-nav">
@@ -43,8 +88,7 @@
     </li>
     <li class="nav-item">
       <a class="nav-link" href="CONTACTO.php">CONTACTO</a>
-    </li>
-    
+    </li>    
   </ul>
 </nav>
 </div>
--- Proyecto Final/login2.php
+++ Proyecto Final/login2.php
@@ -52,12 +52,20 @@
       }
     ?>
 
+
+      <!-- <form action="login.php" method="post">
+        #<p><input name="user" required></p>
+        #<p><input name="password" type="password" required></p>
+        #<p><input type="submit" value="Log In"></p>
+      </form>-->
+
+
 <body>
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
 			<div class="card-header">
-				<h3>Inicio</h3>
+				<h3>Inicio de sesión</h3>
 				<div class="d-flex justify-content-end social_icon">
 					<span><i class="fab fa-facebook-square"></i></span>
 					<span><i class="fab fa-google-plus-square"></i></span>
@@ -65,31 +73,31 @@
 				</div>
 			</div>
 			<div class="card-body">
-				<form action="login.php" method="post">
+				<form>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input name="user" required type="text" class="form-control" placeholder="Usuario">
+						<input type="text" class="form-control" placeholder="username">
 						
 					</div>
 					<div class="input-group form-group">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-key"></i></span>
 						</div>
-						<input name="password" type="password" required class="form-control" placeholder="Contraseña">
+						<input type="password" class="form-control" placeholder="password">
 					</div>
 					<div class="form-group">
-            <input type="submit" value="Siguiente" class="btn float-right login_btn">
+						<input type="submit" value="Login" class="btn float-right login_btn">
 					</div>
 				</form>
 			</div>
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
-					Aún no tienes una cuenta?<a href="#">Registrate</a>
+					Don't have an account?<a href="#">Sign Up</a>
 				</div>
 				<div class="d-flex justify-content-center">
-					<a href="#">Has olvidado tu contraseña?</a>
+					<a href="#">Forgot your password?</a>
 				</div>
 			</div>
 		</div>
